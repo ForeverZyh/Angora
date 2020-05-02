8f5af80b0671ee181863cd926ede7de82d8bdf5f
Yuhao Zhang <zyhzyhzyh@pku.edu.cn>
2020-05-02 16:31:45 -0500

    add more test cases

---
+++ AD-tests/test8/test8.cpp
@@ -0,0 +1,55 @@
+/*
+ *   Test:
+ *     test complicated branch conditions.
+ *      */
+
+#include "stdio.h"
+#include "stdint.h"
+#include "stdlib.h"
+#include "string.h"
+
+#include "../Int.h"
+
+Int  __attribute__ ((noinline))  foo(Int y) {
+    return y - y / 100 - 1233 / y;
+}
+
+int main (int argc, char** argv) {
+    if (argc < 2) return 0;
+
+    FILE *fp;
+    size_t ret;
+
+    fp = fopen(argv[1], "rb");
+
+    if (!fp) {
+          printf("st err\n");
+            return 0;
+    }
+
+    int len = 1;
+    int16_t _x = 0;
+    ret = fread(&_x, sizeof _x, 1, fp);
+    Int x = Int(_x, 1);
+    x.get_dx(-1);
+    fclose(fp);
+    if (ret < len) {
+          printf("input fail \n");
+            return 0;
+    }
+
+    Int ans = x;
+    Int pre = x;
+    for (int i = 0; i < 10; i++)
+    {
+        pre = ans;
+        ans = foo(ans);
+    }
+
+    if (ans == foo(ans) && pre != ans)
+    {
+        abort();
+    }
+
+    return 0;
+}
